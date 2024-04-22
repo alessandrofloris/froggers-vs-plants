7f8544d9c555c2af1cc8a7d3a9b3d74f7a193e75
Alessandro Floris <floris.alessandro99@gmail.com>
2024-04-22 11:37:37 +0200

    curses init

--- src/main.c
+++ src/main.c
@@ -6,7 +6,69 @@
  * 
 */
 
+#include <curses.h>
+
+#include <stdlib.h>
+
+#include <unistd.h>
+
+void curses_end() {
+
+    endwin();
+
+}
+
+
+/**
+ * Inits the curse mode
+ */
+void curses_init() {
+
+    WINDOW* w = initscr(); /* Start curses mode */
+
+    if( w == NULL ) {
+        fprintf(stderr, "[Error] Ncurses init.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    curs_set(0); /* Hide cursor */
+	noecho(); /* Do not show keypresses */
+	cbreak(); /* Do not buffer keypresses */
+
+}
+
+void menu_init() {
+
+    WINDOW* menu;
+
+    menu = newwin(LINES, COLS, 0, 0);
+    
+    if(menu == NULL) {
+        fprintf(stderr, "[Error] Window init.\n");
+        curses_end();
+        exit(EXIT_FAILURE);
+    }
+
+    nodelay(menu, true); /* Input not blocking */
+    keypad(menu, true); /* Enable special keys */
+
+    box(menu, ACS_VLINE, ACS_HLINE); /* Draws a border around the edges of the specified window */
+
+    mvwprintw(menu, LINES/6, (COLS/2)-9, "Froggers Vs Plants");
+
+    wrefresh(menu);
+
+}
+
+
 int main() {
 
-    return 0;
+    curses_init();
+    
+    menu_init();
+
+    usleep(5000000);
+
+    curses_end();
+    
 }
